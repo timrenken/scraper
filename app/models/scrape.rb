e53e70803fa8a74c45329101679a9bf5a96e45d2
Tim Renken <tim@timrenken.io>
2015-10-20 21:02:46 -0500

    updated scrape model

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -10,16 +10,34 @@ class Scrape
       self.title = doc.at("//h1[@itemprop = 'name']").text
       self.hotness = doc.at("//span[@itemprop = 'ratingValue']").text.to_i
       self.image_url = doc.at_css('#movie-image-section img')['src']
-      self.synopsis = doc.css("#movieSynopsis").text
       self.rating = doc.at("//td[@itemprop = 'contentRating']").text
       self.genre = doc.at("//span[@itemprop = 'genre']").text
       self.director = doc.at("//span[@itemprop = 'name']").text
       self.release_date = doc.at("//td[@itemprop = 'datePublished']").text.to_date
       self.runtime = doc.at("//time[@itemprop = 'duration']").text
+      s = doc.css("#movieSynopsis").text
+      if ! s.valid_encoding?
+        s = s.encode("UTF-16be", :invalid=> :replace, :replace=>"?").encode('UTF-8')
+      end
       return true
     rescue Exception => e
       self.failure = "Something went wrong with the scrape!"
     end
+  end
+  
+  def save_movie
+    movie = Movie.new(
+      title: self.title,
+      hotness: self.hotness,
+      image_url: self.image_url,
+      synopsis: self.synopsis,
+      rating: self.rating,
+      genre: self.genre,
+      director: self.director,
+      release_date: self.release_date,
+      runtime: self.runtime
+    )
+    movie.save
   end
   
 end
